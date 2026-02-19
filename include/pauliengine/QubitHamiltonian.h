fceec5e96c1071454e20a0afe9efa44cd89170b8
Ludmila Botelho <ludmila.botelho@algorithmiq.fi>
2026-02-19 12:03:05 +0000

    chore: move changes from another branch here

--- include/pauliengine/QubitHamiltonian.h
+++ include/pauliengine/QubitHamiltonian.h
@@ -172,40 +172,7 @@ class QubitHamiltonian{
     #endif
 
     private:
-    static Matrix2D get_pauli_matrix(const std::string& p){
-        static const std::complex<double> I(0,1);
-        if (p == "I") return {{1,0},{0,1}};
-        else if (p == "X") return {{0,1},{1,0}};
-        else if (p == "Y") return {{0,-I},{I,0}};
-        else if (p == "Z") return {{1,0},{0,-1}};
-        else throw std::runtime_error("Unbekannter Pauli-Operator");
-}
-    static Matrix2D tensor_product(const Matrix2D& A, const Matrix2D& B)
-    {
-        int rows = A.size() * B.size();
-        int cols = A[0].size() * B[0].size();
-        Matrix2D result(rows, std::vector<std::complex<double>>(cols, 0));
-
-        for (int i=0; i<(int)A.size(); ++i) {
-                for (int j=0; j<(int)A[0].size(); ++j) {
-                        for (int k=0; k<(int)B.size(); ++k) {
-                                for (int l=0; l<(int)B[0].size(); ++l) {
-                                        result[i*B.size()+k][j*B[0].size()+l] = A[i][j] * B[k][l];
-                                }
-                        }
-                }
-        }
-        return result;
-}
-    static Matrix2D pauli_string_to_matrix(const std::unordered_map<int, std::string>& pauli_map, int num_qubits){
-    Matrix2D result = get_pauli_matrix("I");
-        for (int q=0; q < num_qubits; ++q) {
-                std::string op = "I";
-                auto it = pauli_map.find(q);
-                if (it != pauli_map.end()) op = it->second;
-                Matrix2D pmat = get_pauli_matrix(op);
-                result = tensor_product(result, pmat);
-        }
-        return result;
-        }
+    static Matrix2D get_pauli_matrix(const std::string& p);
+    static Matrix2D tensor_product(const Matrix2D& A, const Matrix2D& B);
+    static Matrix2D pauli_string_to_matrix(const std::unordered_map<int, std::string>& pauli_map, int num_qubits);
 };
