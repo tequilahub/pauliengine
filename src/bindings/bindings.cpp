bc0447f3147b082855391308572c200cda12d88c
Ludmila Botelho <ludmila.botelho@algorithmiq.fi>
2026-02-25 13:50:52 +0000

    partial fix for now

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -21,27 +21,22 @@ NB_MODULE(_core, m) {
 
         nb::class_<PauliString<std::complex<double>>>(m, "PauliString", "Represents a Pauli string in binary symplectic form.")
                 .def(nb::init<>(), "Default constructor.")
-                .def(nb::init<const std::string, const std::unordered_map<int, std::string>>(),
+                .def(nb::init<std::complex<double>, const std::unordered_map<int, std::string>&>(),
                 "Constructor from a map of qubit indices to Pauli operators and a complex coefficient.")
-                .def(nb::init<std::complex<double>, const std::unordered_map<int, std::string>>(),
-                "Constructor from a map of qubit indices to Pauli operators and a symbolic coefficient.")
                 .def(nb::init<const std::pair<std::complex<double>, std::vector<std::pair<char, int>>>>(),
                 "Constructor OpenFermion style and a complex coefficient.")
-                .def(nb::init<const std::pair<std::string, std::vector<std::pair<char, int>>>>(),
-                "Constructor OpenFermion style and a symbolic coefficient.")
-                .def("to_string", &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
+                // .def("to_string", &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
                 .def("is_all_z", &PauliString<std::complex<double>>::is_all_z, "Checks if the Pauli string consists only of Z operators.")
                 .def("get_coeff", &PauliString<std::complex<double>>::get_coeff, "Returns the complex coefficient of the Pauli string.")
                 .def("to_dictionary", &PauliString<std::complex<double>>::to_dictionary, "Converts the Pauli string to a dictionary with coefficient and operators.")
                 .def("commutator", &PauliString<std::complex<double>>::commutator, "Computes the commutator with another Pauli string.")
                 .def("map_qubits", &PauliString<std::complex<double>>::map_qubits, "Remaps qubit indices according to a given mapping.")
-                .def("diff", &PauliString<std::complex<double>>::diff, "Differentiation after given Variable")
                 .def("__mul__", nb::overload_cast<const PauliString<std::complex<double>>&>(&PauliString<std::complex<double>>::operator*, nb::const_), "Multiply two PauliStrings")
                 .def("__mul__", nb::overload_cast<const std::complex<double>>(&PauliString<std::complex<double>>::operator*), "Scale PauliString by complex scalar")
                 .def("__imul__", nb::overload_cast<const std::complex<double>>(&PauliString<std::complex<double>>::operator*=), "In-place scale")
                 .def("__imul__", nb::overload_cast<const PauliString<std::complex<double>>&>(&PauliString<std::complex<double>>::operator*=), "In-place multiply")
-                .def("__repr__",  &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
-                .def("__str__",  &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
+                // .def("__repr__",  &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
+                // .def("__str__",  &PauliString<std::complex<double>>::to_string, "Returns a human-readable string representation of the Pauli string.")
                 .def("__eq__", &PauliString<std::complex<double>>::equals, "Checks if 2 PauliStrings have same data and Coefficient")
                 .def("diff", &PauliString<std::complex<double>>::key_openfermion, "Returns Paulistring in Openfermion format")
                 .def("qubits", &PauliString<std::complex<double>>::qubits, "Returns list of qubits")
@@ -55,20 +50,18 @@ NB_MODULE(_core, m) {
                 .def_ro("coeff", &PauliString<std::complex<double>>::coeff, "Returns the coefficient of the Pauli string.")
                 .def_ro("is_zero", &PauliString<std::complex<double>>::is_zero, "Returns whether the Pauli string is zero.");
 
-        nb::class_<QubitHamiltonian>(m, "QubitHamiltonian", "Represents a Hamiltonian as a sum of Pauli strings.")
+        nb::class_<QubitHamiltonian<std::complex<double>>>(m, "QubitHamiltonian", "Represents a Hamiltonian as a sum of Pauli strings.")
                 .def(nb::init<const std::vector<PauliString<std::complex<double>>>&>(), "Constructor from a vector of Pauli strings.")
-                .def(nb::init<const Hamiltonian_structure&>(), "Constructor from a Hamiltonian structure (coefficient and operator map).")
-                .def(nb::init<const Hamiltonian_structure_variable&>(), "Constructor from a Hamiltonian structure (coefficient and operator map).")
-                .def("__add__", &QubitHamiltonian::operator+, "Adds two Hamiltonians.")
-                .def("__mul__", nb::overload_cast<std::complex<double> const>(&QubitHamiltonian::operator*, nb::const_), "Scales the Hamiltonian by a complex scalar.")
-                .def("__mul__", nb::overload_cast<QubitHamiltonian const>(&QubitHamiltonian::operator*, nb::const_), "Multiplies two Hamiltonians.")
-                .def("trace_out_qubits", &QubitHamiltonian::trace_out_qubits, "Traces out specified qubits in given states.")
-                .def("to_string", &QubitHamiltonian::to_string, "Converts the Hamiltonian to its full matrix representation.")
-                .def("diff", &QubitHamiltonian::diff, "Differentiation after given Variable")
-                .def("subs", &QubitHamiltonian::substitute, "Replace Variable with value")
-                .def("parse_python_format", &QubitHamiltonian::parse_python_format, "Konvertiert mit SymEngine-Koeffizienten")
-                .def("__str__",&QubitHamiltonian::to_string, "Returns a human-readable string representation of the QubitHamiltonian operator.")
-                .def("__repr__",&QubitHamiltonian::to_string, "Returns a human-readable string representation of the QubitHamiltonian operator.");
+                .def(nb::init<const Hamiltonian_structure<std::complex<double>>&>(), "Constructor from a Hamiltonian structure (coefficient and operator map).")
+                .def("__add__", &QubitHamiltonian<std::complex<double>>::operator+, "Adds two Hamiltonians.")
+                .def("__mul__", nb::overload_cast<std::complex<double> const>(&QubitHamiltonian<std::complex<double>>::operator*, nb::const_), "Scales the Hamiltonian by a complex scalar.")
+                .def("__mul__", nb::overload_cast<QubitHamiltonian<std::complex<double>> const>(&QubitHamiltonian<std::complex<double>>::operator*, nb::const_), "Multiplies two Hamiltonians.")
+                .def("trace_out_qubits", &QubitHamiltonian<std::complex<double>>::trace_out_qubits, "Traces out specified qubits in given states.")
+                // .def("to_string", &QubitHamiltonian<std::complex<double>>::to_string, "Converts the Hamiltonian to its full matrix representation.")
+                // .def("subs", &QubitHamiltonian<std::complex<double>>::substitute, "Replace Variable with value")
+                .def("parse_python_format", &QubitHamiltonian<std::complex<double>>::parse_python_format, "Konvertiert mit SymEngine-Koeffizienten");
+                // .def("__str__",&QubitHamiltonian<std::complex<double>>::to_string, "Returns a human-readable string representation of the QubitHamiltonian operator.")
+                // .def("__repr__",&QubitHamiltonian<std::complex<double>>::to_string, "Returns a human-readable string representation of the QubitHamiltonian operator.");
 
 
         nb::class_<SymEngine::Expression>(m, "Expression")
